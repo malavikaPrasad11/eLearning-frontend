fb29f33bf32c6069eec64b471d3fc7962455d935
Malavika S Prasad <malavikasprasad@gmail.com>
2025-10-07 19:30:23 +0530

    Javascript.jsx

--- src/Components/Course/Javascript.jsx
+++ src/Components/Course/Javascript.jsx
@@ -14,7 +14,7 @@ export default function Javascript() {
                 <div className="container">
                     <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
                         <h6 className="section-title bg-white text-center text-primary px-3">Courses</h6>
-                        <h1 className="mb-5">Javascript full course Tutorial</h1>
+                        <h1 className="mb-5">Javascript Full Course Tutorial</h1>
                     </div>
                     <div className="row g-2 justify-content-center">
 
