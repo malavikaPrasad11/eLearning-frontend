0c3bc79db78a136fc97a138d72c3ab5a9075205d
Malavika S Prasad <malavikasprasad@gmail.com>
2025-10-07 19:31:48 +0530

    Express.jsx

--- src/Components/Course/Express.jsx
+++ src/Components/Course/Express.jsx
@@ -10,7 +10,7 @@ export default function EXpress() {
              <div className="container-xxl py-5">
                 <div className="container">
                     <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-                        <h6 className="section-title bg-white text-center text-primary px-3">Courses</h6>
+                        <h6 className="section-title bg-white text-center text-primary px-3">Course</h6>
                         <h1 className="mb-5">Programming Languages Tutorials</h1>
                     </div>
                     <div className="row g-2 justify-content-center">
